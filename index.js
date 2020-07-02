0f2834dd717a54a331d989b3accc0664c1529c7e
vishnuramesh <vishnuramesh19@gmail.com>
2020-07-02 21:10:54 +0530

    added body-parser middleware

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const app = express()
 // middleware - logger
 const logger = (req,res,next) => {
 	console.log(`log : ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+	// console.log(JSON.stringify(req.body))
 	next()
 }
 
 // initializing the logger middleware
 app.use(logger)
+
+// initializing body parser
+app.use(express.json())
+// for handling url encoded data. ie html forms
+app.use(express.urlencoded({ extended:false }))
+
+// test
+app.post('/testbody' ,(req,res) => {
+	res.send(req.body)
+})
 
 app.get('/', (req,res) => {
 	res.send("<h1>hello world!!</h1>")
@@ -25,6 +36,11 @@ app.get('/api', (req,res) => {
 // post with param in url
 app.post('/api/:id', (req,res) => {
 	res.json(req.params.id)
+})
+
+// get an error with status 400
+app.post('/apierror', (req,res) => {
+	res.status(400).json({error:`boom an error. Hope you like it`})
 })
 
 const PORT = process.env.PORT || 5000
