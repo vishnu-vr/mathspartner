16387eea50b59a84d3d1bf160e7dda88dce5d5b9
vishnuramesh <vishnuramesh19@gmail.com>
2020-07-02 20:47:33 +0530

    simple post and get apis

--- index.js
+++ index.js
@@ -2,10 +2,66 @@ const express = require('express')
 
 const app = express()
 
+// middleware - logger
+const logger = (req,res,next) => {
+	console.log(`log : ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+	next()
+}
+
+// initializing the logger middleware
+app.use(logger)
+
 app.get('/', (req,res) => {
 	res.send("<h1>hello world!!</h1>")
+})
+
+var dic = {name:'vishnu',age:'21'}
+
+// api tests
+// get
+app.get('/api', (req,res) => {
+	res.json(dic)
+})
+// post with param in url
+app.post('/api/:id', (req,res) => {
+	res.json(req.params.id)
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
