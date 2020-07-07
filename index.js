1f23225592157da9d7acb38fb78f636111ec7a66
vishnuramesh <vishnuramesh19@gmail.com>
2020-07-07 23:56:40 +0530

    // case - 3 : topic name and part number doesn't exists (ie we are adding a tottaly new quiz)

--- index.js
+++ index.js
@@ -208,6 +208,49 @@ app.post('/add_quiz', (req,res) => {
 		});
 	
 	}
+	// case - 3 : topic name and part number doesn't exists (ie we are adding a totally new quiz)
+	else if (data[0].topic_name_exists == false && data[0].part_number_exists == false){
+		// first thing to do is : create a new row in index_table 
+		// with topic name and part number
+		var part_number = 'part_' + data[0].part_number
+		con.query("INSERT INTO index_table VALUES (NULL, '"+data[0].topic_name+"', '"+part_number+"')", function (err, result, fields) {
+			if (err) throw err;
+			// console.log(result)
+			// after creating a new row in index table we have to
+			// create an info-table of that part
+			var table_name = data[0].topic_name + part_number
+			// console.log(table_name)
+			var sql = "CREATE TABLE "+table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, question_paper VARCHAR(20), duration VARCHAR(20))";
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+				console.log("Table created");
+				// after creating the info-table, add the first entry
+				// with id, question-paper and then duration
+				var sql = "INSERT INTO "+table_name+" VALUES (1 ,'"+data[0].question_paper+"', '"+data[0].duration+"')";
+				con.query(sql, function (err, result) {
+					if (err) throw err;
+					console.log("1 record inserted");
+					// after all that, create the actual table containing
+					// questions and insert the questions, options and
+					// correct answer
+					table_name += data[0].question_paper
+					var sql = "CREATE TABLE "+table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), option_1 VARCHAR(20), option_2 VARCHAR(20), option_3 VARCHAR(20), option_4 VARCHAR(20), correct VARCHAR(20))";
+					con.query(sql, function (err, result) {
+						if (err) throw err;
+						console.log("Table created");
+						// insert everythin
+						for (var i=0; i<data.length; i++){
+							var sql = "INSERT INTO "+table_name+" VALUES ("+(i+1)+", '"+data[i].question+"', '"+data[i].options[0]+"', '"+data[i].options[1]+"', '"+data[i].options[2]+"', '"+data[i].options[3]+"', '"+data[i].correct+"')";
+							con.query(sql, function (err, result) {
+							if (err) throw err;
+							console.log("row " +(i+1)+" inserted");
+							});
+						}
+					});
+				});
+			});
+		});
+	}
 
 	res.json("add_quiz api called")
 })
--- test.js
+++ test.js
@@ -12,82 +12,73 @@ if (err) throw err;
 console.log("Database Connected!");
 });
 
-// case - 2 : topic name exists and part number doesn't exists (ie we are adding a 
-// new question_paper and a part-info table)
+// case - 3 : topic name and part number doesn't exists (ie we are adding a tottaly new quiz)
 var data = [
   {
-    topic_name: 'RATIO',
+    topic_name: 'NEW_TOPIC',
     part_number: '4',
     question_paper: 'question_paper_1',
     question: 'asd',
     options: [ 'asd', 'asd', 'asd', 'asd' ],
     correct: 'asd',
     duration: '22',
-    topic_name_exists: true,
+    topic_name_exists: false,
     part_number_exists: false
   },
   {
-    topic_name: 'RATIO',
+    topic_name: 'NEW_TOPIC',
     part_number: '4',
     question_paper: 'question_paper_1',
     question: 'asd',
     options: [ 'asd', 'asd', 'asd', 'asd' ],
     correct: 'asd',
     duration: '22',
-    topic_name_exists: true,
+    topic_name_exists: false,
     part_number_exists: false
   }
 ]
 // console.log("asd")
-if (data[0].topic_name_exists && data[0].part_number_exists == false){
-	// first thing to do is : update the index_table 
-	// with the new part number
-	// for that we initially fetch the parts
-	// and append the new part with '#'
+if (data[0].topic_name_exists == false && data[0].part_number_exists == false){
+	// first thing to do is : create a new row in index_table 
+	// with topic name and part number
 	var part_number = 'part_' + data[0].part_number
-	con.query("SELECT parts FROM index_table WHERE topic_name = '"+data[0].topic_name+"'", function (err, result, fields) {
+	con.query("INSERT INTO index_table VALUES (NULL, '"+data[0].topic_name+"', '"+part_number+"')", function (err, result, fields) {
 		if (err) throw err;
-		var updated_parts = result[0].parts + "#" + part_number
-		// console.log(updated_parts);
-		con.query("UPDATE index_table SET parts = '"+updated_parts+"' WHERE topic_name = '"+data[0].topic_name+"'", function (err, result, fields) {
+		// console.log(result)
+		// after creating a new row in index table we have to
+		// create an info-table of that part
+		var table_name = data[0].topic_name + part_number
+		// console.log(table_name)
+		var sql = "CREATE TABLE "+table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, question_paper VARCHAR(20), duration VARCHAR(20))";
+		con.query(sql, function (err, result) {
 			if (err) throw err;
-			// console.log(result)
-			// after updating index table we have to
-			// create an info-table of that part
-			var table_name = data[0].topic_name + part_number
-			// console.log(table_name)
-			var sql = "CREATE TABLE "+table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, diff_level VARCHAR(20), duration VARCHAR(20))";
+			console.log("Table created");
+			// after creating the info-table, add the first entry
+			// with id, question-paper and then duration
+			var sql = "INSERT INTO "+table_name+" VALUES (1 ,'"+data[0].question_paper+"', '"+data[0].duration+"')";
 			con.query(sql, function (err, result) {
 				if (err) throw err;
-				console.log("Table created");
-				// after creating the info-table, add the first entry
-				// with id, question-paper and then duration
-				var sql = "INSERT INTO "+table_name+" VALUES (1 ,'"+data[0].question_paper+"', '"+data[0].duration+"')";
+				console.log("1 record inserted");
+				// after all that, create the actual table containing
+				// questions and insert the questions, options and
+				// correct answer
+				table_name += data[0].question_paper
+				var sql = "CREATE TABLE "+table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), option_1 VARCHAR(20), option_2 VARCHAR(20), option_3 VARCHAR(20), option_4 VARCHAR(20), correct VARCHAR(20))";
 				con.query(sql, function (err, result) {
 					if (err) throw err;
-					console.log("1 record inserted");
-					// after all that, create the actual table containing
-					// questions and insert the questions, options and
-					// correct answer
-					table_name += data[0].question_paper
-					var sql = "CREATE TABLE "+table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), option_1 VARCHAR(20), option_2 VARCHAR(20), option_3 VARCHAR(20), option_4 VARCHAR(20), correct VARCHAR(20))";
-					con.query(sql, function (err, result) {
+					console.log("Table created");
+					// insert everythin
+					for (var i=0; i<data.length; i++){
+						var sql = "INSERT INTO "+table_name+" VALUES ("+(i+1)+", '"+data[i].question+"', '"+data[i].options[0]+"', '"+data[i].options[1]+"', '"+data[i].options[2]+"', '"+data[i].options[3]+"', '"+data[i].correct+"')";
+						con.query(sql, function (err, result) {
 						if (err) throw err;
-						console.log("Table created");
-						// insert everythin
-						for (var i=0; i<data.length; i++){
-							var sql = "INSERT INTO "+table_name+" VALUES ("+(i+1)+", '"+data[i].question+"', '"+data[i].options[0]+"', '"+data[i].options[1]+"', '"+data[i].options[2]+"', '"+data[i].options[3]+"', '"+data[i].correct+"')";
-							con.query(sql, function (err, result) {
-							if (err) throw err;
-							console.log("row " +(i+1)+" inserted");
-							});
-						}
-					});
+						console.log("row " +(i+1)+" inserted");
+						});
+					}
 				});
 			});
 		});
 	});
-
 }
 
 
