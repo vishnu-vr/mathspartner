cc534f2cfc238e8b0edb66a8a1a0ff8a528a56e0
vishnuramesh <vishnuramesh19@gmail.com>
2020-10-08 05:07:58 +0530

    same

--- test.js
+++ test.js
@@ -19,6 +19,28 @@ new_con.query("DELETE FROM `user_details` WHERE `user_details`.`id` = ?", [10], 
 	console.log(result);
 })
 
+// SELECT * FROM gk WHERE parent LIKE 'QUESTION PAPERS-ENGLISH-PHOBIAS%';
+
+
+
+// create a new folder name "PHOBIAS"
+
+// insert gk table entries where parent = "QUESTION PAPERS-ENGLISH-PHOBIAS" and child = "1"
+// INSERT INTO gk (parent, child, type) VALUES ("QUESTION PAPERS-ENGLISH-PHOBIAS", "1", "quiz");
+
+// rename "QUESTION PAPERS-ENGLISH-PHOBIAS 1" to "QUESTION PAPERS-ENGLISH-PHOBIAS-1"
+// UPDATE gk SET parent = "QUESTION PAPERS-ENGLISH-PHOBIAS-1" WHERE parent = "QUESTION PAPERS-ENGLISH-PHOBIAS 1"
+
+// rename the quiz table "QUESTION PAPERS-ENGLISH-PHOBIAS 1" to "QUESTION PAPERS-ENGLISH-PHOBIAS-1"
+// ALTER TABLE `QUESTION PAPERS-ENGLISH-PHOBIAS 1` RENAME TO `QUESTION PAPERS-ENGLISH-PHOBIAS-1`;
+
+
+// delete gk entry where parent = "QUESTION PAPERS-ENGLISH" and child = "PHOBIAS 1"
+// DELETE FROM gk WHERE parent = "QUESTION PAPERS-ENGLISH" AND child = "PHOBIAS 1";
+
+
+
+
 // // will return list of unique quiz names
 // new_con.query("SELECT DISTINCT `quiz_name` FROM `user_details`", function(err,result,fields){
 //   if (err){
