967aa79f96ab077ce491726abe594a5d897ee31e
vishnuramesh <vishnuramesh19@gmail.com>
2020-10-08 00:54:20 +0530

    same

--- backup.py
+++ backup.py
@@ -1,8 +1,6 @@
 import os
 import sys
 import datetime
-import schedule
-import time
 import json
 
 with open('/home/ubuntu/cred.json') as f:
@@ -24,14 +22,4 @@ def backup():
 	os.system("git commit -m '"+ today +"'")
 	os.system("git push origin master")
 
-# schedule.every(1).minutes.do(backup)
-# schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(backup)
-# schedule.every(5).to(10).minutes.do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-# schedule.every().minute.at(":17").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+backup()
